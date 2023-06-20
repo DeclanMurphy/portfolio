e7cd4161fbee54a9233627c4ad31ef8b6ec9d8f4
Declan Murphy <deccy06@gmail.com>
2023-06-20 18:35:01 +0200

    Update to NP env var

--- src/lib/getUrl.ts
+++ src/lib/getUrl.ts
@@ -1,6 +1,6 @@
 import { cache } from 'react';
 export const getUrl = cache(() =>
-  process.env.VERCEL_URL
+  process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://declan-murphy.vercel.app`
     : `http://localhost:${process.env.PORT ?? 3000}`
 );
